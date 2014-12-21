8dd55497e2b15811ffd9acaab697b7cadd541153
Zoadian <github@zoadian.de>
2014-12-21 19:47:07 +0100

    fixed location order in shader code

--- source/gl/renderer.d
+++ source/gl/renderer.d
@@ -148,8 +148,8 @@ enum vertexShaderSource = "
 
 	layout(location = 0) in vec3 in_position;
 	layout(location = 1) in vec3 in_normal;
-	layout(location = 2) in vec2 in_texcoord;
-	layout(location = 3) in vec4 in_color;
+	layout(location = 2) in vec4 in_color;
+	layout(location = 3) in vec2 in_texcoord;
 
 	//==============
 	out vec2 _normal;
